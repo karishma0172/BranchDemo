fe129db4a084653892f9cb5021ab9b6cf2fd097b
Karishma <karishma.172k@gmail.com>
2021-11-17 09:58:14 +0530

    Add project files

--- BranchDemo/Program.cs
+++ BranchDemo/Program.cs
@@ -7,6 +7,7 @@ namespace BranchDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Master Branch Created");
         }
     }
 }
